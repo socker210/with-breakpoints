b79a9d979ef72c48fa44aa870bde111cff15756b
socker210 <socker210@gmail.com>
2019-06-04 10:38:43 +0900

    Add docz documents

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  title: 'with-breakponts',
+  description: 'CSS media queries in react, for build a responsive design as you want',
+  indexHtml: path.resolve(__dirname, 'docz', 'index.html'),
+  host: '0.0.0.0',
+  port: 3003,
+  menu: [
+    'Getting started',
+    'Examples'
+  ]
+}
