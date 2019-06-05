06adfc71de079b7b79859b6cf4f6973012a404b5
socker210 <socker210@gmail.com>
2019-06-05 17:44:25 +0900

    Rebuild docs

--- doczrc.js
+++ doczrc.js
@@ -3,10 +3,10 @@ const path = require('path')
 const pkg = require('./package.json')
 
 module.exports = {
-  hashRouter: true,
   title: pkg.name,
   description: pkg.description,
   indexHtml: path.resolve(__dirname, 'docz', 'index.html'),
+  base: `/${pkg.name}/`,
   dest: 'docs',
   host: '0.0.0.0',
   port: 3003,
