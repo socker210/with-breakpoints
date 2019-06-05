152fc25e5e49dbc5d03e68e8f4cc592855735f27
socker210 <socker210@gmail.com>
2019-06-05 19:51:50 +0900

    Remove docs on master branch, Add gh-pages

--- doczrc.js
+++ doczrc.js
@@ -1,10 +1,8 @@
-const path = require('path')
 const pkg = require('./package.json')
 
 module.exports = {
   title: pkg.name,
   description: pkg.description,
-  indexHtml: path.resolve(__dirname, 'docz', 'index.html'),
   base: `/${pkg.name}/`,
   dest: 'docs',
   host: '0.0.0.0',
