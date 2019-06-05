04d113bc2b56292ab48811503c05dfc574f86046
socker210 <socker210@gmail.com>
2019-06-05 17:04:58 +0900

    Add docs

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,7 @@ module.exports = {
   title: 'with-breakponts',
   description: 'CSS media queries in react, for build a responsive design as you want',
   indexHtml: path.resolve(__dirname, 'docz', 'index.html'),
+  dest: 'docs',
   host: '0.0.0.0',
   port: 3003,
   menu: [
